8c4e2ed86eafa1b634a28d129ee8ecbee88b8916
kickscar <kickscar@gmail.com>
2016-11-11 13:46:53 +0900

    JSONResult DTO 적용

--- src/main/java/com/bit2016/mysite/controller/api/GuestbookController.java
+++ src/main/java/com/bit2016/mysite/controller/api/GuestbookController.java
@@ -1,9 +1,6 @@
 package com.bit2016.mysite.controller.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bit2016.dto.JSONResult;
 import com.bit2016.mysite.service.GuestbookService;
 import com.bit2016.mysite.vo.GuestbookVo;
 
@@ -24,40 +22,24 @@ public class GuestbookController {
 	
 	@ResponseBody
 	@RequestMapping( "/list" )
-	public Map<String, Object> list(
+	public JSONResult list(
 		@RequestParam( value="p", required=true, defaultValue="1" ) Integer page ){
-		
+
 		List<GuestbookVo> list = guestbookService.getMessageList(page);
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put( "result", "success" );
-		map.put( "data", list );
-		
-		return map;
+		return JSONResult.success( list );
 	}
 	
 	@ResponseBody
 	@RequestMapping( value="/add", method=RequestMethod.POST )
-	public Map<String, Object> add( @ModelAttribute GuestbookVo vo ){
-		
+	public JSONResult add( @ModelAttribute GuestbookVo vo ){
 		GuestbookVo guestbookVo = guestbookService.writeMessage(vo, true);
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put( "result", "success" );
-		map.put( "data", guestbookVo );
-		
-		return map;
+		return JSONResult.success( guestbookVo );
 	}
 	
 	@ResponseBody
 	@RequestMapping( value="/delete" )
-	public Map<String, Object> delete( @ModelAttribute GuestbookVo vo ){
+	public JSONResult delete( @ModelAttribute GuestbookVo vo ){
 		boolean result = guestbookService.deleteMessage( vo );
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put( "result", "success" );
-		map.put( "data", result ? vo.getNo() : -1 );
-		
-		return map;
+		return JSONResult.success( result ? vo.getNo() : -1 );
 	}	
 }
